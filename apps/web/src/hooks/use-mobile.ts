e4b54d83e9c4d64cc74e297cb00fb4fa85408026
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2026-08-17 07:36:39 -0500

    fix: address dependabot review comments

--- apps/web/src/hooks/use-mobile.ts
+++ apps/web/src/hooks/use-mobile.ts
@@ -5,8 +5,15 @@ const MOBILE_QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
 
 function subscribe(onStoreChange: () => void) {
   const mql = window.matchMedia(MOBILE_QUERY)
-  mql.addEventListener("change", onStoreChange)
-  return () => mql.removeEventListener("change", onStoreChange)
+  const handleChange = () => onStoreChange()
+
+  if (typeof mql.addEventListener === "function") {
+    mql.addEventListener("change", handleChange)
+    return () => mql.removeEventListener("change", handleChange)
+  }
+
+  mql.addListener(handleChange)
+  return () => mql.removeListener(handleChange)
 }
 
 function getSnapshot() {
